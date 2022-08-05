30df941d56cf5ff38f89a8ced8c1a72a72c93e9e
Matt Crump <crumpmj@gmail.com>
2022-08-05 15:38:08 -0400

    quarto version
    convert to quarto book

---
+++ global_stuff.R
@@ -0,0 +1,10 @@
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>",
+  message = FALSE,
+  warning = FALSE,
+  cache = TRUE,
+  cache.path = "cache/",
+  out.width = '75%',
+  echo = FALSE
+)
